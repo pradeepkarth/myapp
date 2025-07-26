5a43cfee1141c0ef56b58d93f2aa614dcbf6496f
pradeepkarth <pradeepkarth@gmail.com>
2025-07-26 20:49:22 +0000

    chore: App name changed

--- lib/core/utils/strings.dart
+++ lib/core/utils/strings.dart
@@ -1,7 +1,6 @@
 class AppStrings {
-  static const String appTitle = 'Aquacart';
+  static const String appTitle = 'Nammai Meenagam';
   static const String fishImageAltText = 'Fish Image';
   static const String errorPrefix = 'Error: ';
   static const String pressButtonToLoadImages = 'Press button to load images';
-  // Remove offerCarouselImages here, keep only UI strings.
 }
