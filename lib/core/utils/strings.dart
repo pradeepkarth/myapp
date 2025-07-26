435ab32bda15e2bde30bfcfd7acdaf2c69dded81
pradeep.arjunan <pradeep.arjunan@jungleegmaes.com>
2025-07-27 04:16:58 +0530

    reverse: Checking pipeline deployment

--- lib/core/utils/strings.dart
+++ lib/core/utils/strings.dart
@@ -1,5 +1,5 @@
 class AppStrings {
-  static const String appTitle = 'Nammai Meenagam';
+  static const String appTitle = 'Github actions';
   static const String galleryTitle = 'Gallery';
   static const String fishImageAltText = 'Fish Image';
   static const String errorPrefix = 'Error: ';
